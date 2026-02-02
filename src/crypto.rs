c53799e731a320dfbb8e1053f9001245ed711520
Akshat-Kalra <akshatkalra2005@gmail.com>
2026-02-01 20:04:16 -0800

    Add encryption and decryption functions using AES-GCM

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,6 +1,9 @@
 use crate::error::{RustlockError, Result};
 use argon2::{Argon2, Params};
-    
+use aes_gcm::{Aes256Gcm, KeyInit, Nonce};
+use aes_gcm::aead::Aead;
+use rand::RngCore;
+
 pub fn derive_key(password: &str, salt: &[u8]) -> Result<[u8; 32]> {
     let params = match Params::new(64 * 1024, 3, 1, Some(32)) {
         Ok(p) => p,
@@ -17,4 +20,54 @@ pub fn derive_key(password: &str, salt: &[u8]) -> Result<[u8; 32]> {
     };
 
     Ok(key)
+}
+
+
+pub fn encrypt(key: &[u8; 32], plaintext:&[u8]) -> Result<Vec<u8>> {
+
+    let mut nonce = [0u8; 12];
+    rand::rng().fill_bytes(&mut nonce);
+
+    let nonce_obj = Nonce::from_slice(&nonce);
+
+    let cipher = match Aes256Gcm::new_from_slice(key) {
+        Ok(p) => p,
+        Err(p) => return Err(RustlockError::Crypto(p.to_string())),
+    };
+
+    let ciphertext = match cipher.encrypt(nonce_obj, plaintext) {
+        Ok(p) => p,
+        Err(p) => return Err(RustlockError::Crypto(p.to_string())),
+    };
+
+    let mut result = nonce.to_vec();
+    result.extend(ciphertext);
+
+    Ok(result)
+
+}
+
+pub fn decrypt(key: &[u8; 32], ciphertext: &[u8]) -> Result<Vec<u8>> {
+    if ciphertext.len() < 12 {
+        return Err(RustlockError::Crypto("Text is shorter than nonce".to_string()));
+    }
+
+    let (nonce_bytes, actual_ciphertext) = ciphertext.split_at(12);
+
+    let nonce = Nonce::from_slice(nonce_bytes);
+
+    let cipher = match Aes256Gcm::new_from_slice(key) {
+        Ok(p) => p,
+        Err(p) => return Err(RustlockError::Crypto(p.to_string())),
+    };
+
+    let plaintext = match cipher.decrypt(nonce, actual_ciphertext) {
+        Ok(p) => p,
+        Err(p) => return Err(RustlockError::Crypto(p.to_string())),
+    };
+
+    Ok(plaintext)
+    
+
+
 }
