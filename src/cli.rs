6dece641b1f395dd328478406a0ad20a0137db34
Akshat-Kalra <akshatkalra2005@gmail.com>
2026-02-01 22:25:25 -0800

    Implement CLI structure and command handling for the password manager

---
+++ src/cli.rs
@@ -0,0 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "rustlock")]
+#[command(about = "A local password manager")]
+
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Add {
+        website: String,
+        username: String
+    },
+    Get {
+        website: String
+    },
+    List,
+    Generate {
+        length: usize,
+    },
+
+    }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,37 @@ mod password;
 mod crypto;
 mod vault;
 mod storage;
+mod cli;
+
+use cli::{Commands, Cli};
+use error::Result;
+use clap::Parser;
 
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+          eprintln!("Error: {}", e);
+          std::process::exit(1);
+      }
+}
+
+fn run() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add { website, username } => {
+            println!("hello")
+        }
+        Commands::Get { website } => {
+            // Handle get command
+        }
+        Commands::List => {
+            // Handle list command
+        }
+        Commands::Generate { length } => {
+            let pw = password::generate_password(length, true, true, true)?;
+            println!("Generated password: {}", pw);
+        }
+    }
+
+    Ok(())
 }
